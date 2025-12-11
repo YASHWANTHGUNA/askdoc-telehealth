f777d43e5048128559e47a80ddfc34643d453fb1
YASHWANTH <mail2yashwanth2004@gmail.com>
2025-12-11 14:49:49 +0530

    Wrap App in StreamProvider and fix Login route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { StreamProvider } from './contexts/StreamContext';
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+// 1. IMPORT THE PROVIDER
+import { StreamProvider } from './StreamContext.jsx' 
 
-// Notice: No <StrictMode> tags anymore
-createRoot(document.getElementById('root')).render(
-  <StreamProvider>
-    <App />
-  </StreamProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      {/* 2. WRAP THE APP INSIDE THE PROVIDER */}
+      <StreamProvider>
+        <App />
+      </StreamProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
